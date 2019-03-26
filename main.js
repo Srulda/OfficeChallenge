7987f864509c104e6cfd0f5dc4d330e82abe0567
Srulda <48174460+Srulda@users.noreply.github.com>
2019-03-26 14:08:01 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,2 +1,24 @@
-//write your code here to make the tests pass
 
+$("button").on("click", function () {
+    let address = $("#addr-input").val()
+    let minPrice = $("#min-p-input").val()
+    let maxPrice = $("#max-p-input").val()
+    let minRooms = $("#min-r-input").val()
+    let maxRooms = $("#max-r-input").val()
+    let immediate = $("#immed-y")
+
+    let relevantApts = findRelevantApts(address, minPrice, maxPrice, minRooms, maxRooms, immediate)
+    renderApts(relevantApts)
+})
+
+const renderApts = function (apartments) {
+    $("#results").empty()
+    console.log(apartments) //array of apartments to render
+    //Your code goes here.
+    let source = $("#apartment-template").html()
+    let template = Handlebars.compile(source)
+    let newHTML = template({apartments})
+    $("#results").append(newHTML)
+}
+
+renderApts(apartments) //renders apartments when page loads
